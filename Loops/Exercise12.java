3c893255954021af24d95b4b5918859347b3b1c1
Nandini-Dhanalakota <140742261+Nandini-Dhanalakota@users.noreply.github.com>
2023-10-12 22:22:14 +0530

    Update Exercise12.java

--- Loops/Exercise12.java
+++ Loops/Exercise12.java
@@ -30,3 +30,29 @@ public class Solution
         }
     }
 }
+
+
+//Solution-2
+import java.util.Scanner;
+public class Solution
+{
+    public static void main(String... args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int h = sc.nextInt();
+
+        for(int i = 1; i <= h; i++)
+        {
+            for(int j = h-1; j >= i; j--)
+            {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= i; k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(); //new line
+        }
+    }
+}
