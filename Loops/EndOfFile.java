fe5d5236efec7bd7baa385212c75bf71c3730456
Nandini-Dhanalakota <140742261+Nandini-Dhanalakota@users.noreply.github.com>
2023-12-17 16:47:46 +0530

    Create EndOfFile.java

---
+++ Loops/EndOfFile.java
@@ -0,0 +1,26 @@
+/* Sample Input
+
+Hello world
+I am a file
+Read me until end-of-file.
+Sample Output
+
+1 Hello world
+2 I am a file
+3 Read me until end-of-file.   */
+
+import java.util.*;
+public class om
+  {
+    public static void main(String... args)
+    {
+      Scanner sc = new Scanner (System.in);
+      int i = 1;
+      while(sc.hasNext())
+        {
+          String s = sc.nextLine();
+          System.out.println(i + " " + s);
+          i++;
+        }
+    }
+  }
