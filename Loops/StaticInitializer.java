fba43e939b1577c45fd90771b2fb91f5d06edbb6
Nandini-Dhanalakota <140742261+Nandini-Dhanalakota@users.noreply.github.com>
2023-12-17 18:00:27 +0530

    Create StaticInitializer.java

---
+++ Loops/StaticInitializer.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+//Write your code here
+static boolean flag =  true;
+static int B;
+static int H;
+
+static 
+{
+    try
+    {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        if(B<=0 || H<=0)
+        {
+            flag = false;
+            throw new Exception("Breadth and height must be positive");
+        }
+    }
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
